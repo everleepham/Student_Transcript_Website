b4599d4fd27a1dfbaaaece7c522ac8168da0e78a
Everlee <khanhpham2101@gmail.com>
2024-02-08 23:31:01 +0100

    sorry dương

--- project.py
+++ project.py
@@ -1,6 +1,9 @@
 import jaydebeapi
 
-for i in range(1, 10):
-    if i < n and i == 0:
+def connect(db_driver_path: str, db_url, db_user: str, db_password: str) -> jaydebeapi.Connection:
+    conn = jaydebeapi.connect(
+        "org.h2.Driver", db_driver_path, db_url, [db_user, db_password])
+    return conn
+
 
 
