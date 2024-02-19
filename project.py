1874d531ffc3c5a62ce1dda9d3e5de6e848ca71f
Everlee <khanhpham2101@gmail.com>
2024-02-19 15:01:23 +0100

    Deleted file

--- project.py
+++
@@ -1,9 +0,0 @@
-import jaydebeapi
-
-def connect(db_driver_path: str, db_url, db_user: str, db_password: str) -> jaydebeapi.Connection:
-    conn = jaydebeapi.connect(
-        "org.h2.Driver", db_driver_path, db_url, [db_user, db_password])
-    return conn
-
-
-
